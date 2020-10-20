f8f2f03b942e1a6193ea678d6622d3c9e59e9b3d
Admin <maxich66@gmail.com>
2020-10-20 17:27:00 +0300

    add new chromedriver

--- src/test/java/modal/workouts/BaseWorkout.java
+++ src/test/java/modal/workouts/BaseWorkout.java
@@ -1,6 +1,20 @@
 package modal.workouts;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class BaseWorkout {
+
     String date;
-    String time;
+    String timeOfDay;
+    String workoutName;
+    String description;
+    String duration;
+    int distance;
+
 }
---
+++ src/test/java/modal/workouts/Bike.java
@@ -0,0 +1,11 @@
+package modal.workouts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Builder
+public class Bike extends BaseWorkout {
+String ses;
+    }
