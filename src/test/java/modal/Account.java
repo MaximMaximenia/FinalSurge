b51986bc375650fdd4058d56031119e305d37dbc
Admin <maxich66@gmail.com>
2020-10-21 10:25:05 +0300

    delete lombok, log4j dependencies

--- src/test/java/modal/Account.java
+++ src/test/java/modal/Account.java
@@ -1,15 +1,69 @@
 package modal;
 
-import lombok.Data;
 
-@Data
 public class Account {
     String firsName;
     String lastName;
     String email;
     String password;
+
+    public Account(String firsName, String lastName, String email, String password, String reTypePassword, String timeZone) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.reTypePassword = reTypePassword;
+        this.timeZone = timeZone;
+    }
+
     String reTypePassword;
     String timeZone;
 
+    public String getFirsName() {
+        return firsName;
+    }
 
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getReTypePassword() {
+        return reTypePassword;
+    }
+
+    public void setReTypePassword(String reTypePassword) {
+        this.reTypePassword = reTypePassword;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
 }
--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,7 +1,7 @@
 package utils;
 
 import io.qameta.allure.Attachment;
-import lombok.extern.log4j.Log4j2;
+
 import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -11,28 +11,28 @@ import org.testng.ITestResult;
 
 import java.util.concurrent.TimeUnit;
 
-@Log4j2
+
 public class TestListener implements ITestListener {
 
     public void onTestStart(ITestResult iTestResult) {
-        log.info(("======================================== STARTING TEST " + iTestResult.getName() + " ========================================"));
+        System.out.println(("======================================== STARTING TEST " + iTestResult.getName() + " ========================================"));
 
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-        log.info(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        System.out.println(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
 
     }
 
     public void onTestFailure(ITestResult iTestResult) {
-        log.info(String.format("======================================= FAILED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        System.out.println(String.format("======================================= FAILED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
         takeScreenshot(iTestResult);
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
-        log.info(String.format("======================================== SKIPPING TEST %s ========================================%n", iTestResult.getName()));
+        System.out.println(String.format("======================================== SKIPPING TEST %s ========================================%n", iTestResult.getName()));
         takeScreenshot(iTestResult);
     }
 
