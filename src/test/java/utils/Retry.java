2fe5af83d8b9815247397ef5d7d0e98037b7e9cd
Admin <maxich66@gmail.com>
2020-10-21 10:55:34 +0300

    add pom from sauceDemo

--- src/test/java/utils/Retry.java
+++ src/test/java/utils/Retry.java
@@ -4,7 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-@Log4j2
+
 public class Retry implements IRetryAnalyzer {
 
     private int attempt = 1;
@@ -16,7 +16,7 @@ public class Retry implements IRetryAnalyzer {
             if (attempt < MAX_RETRY) {
                 attempt++;
                 iTestResult.setStatus(ITestResult.FAILURE);
-                log.info("Retrying once again");
+
                 return true;
             } else {
                 iTestResult.setStatus(ITestResult.FAILURE);
--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -16,24 +16,20 @@ import java.util.concurrent.TimeUnit;
 public class TestListener implements ITestListener {
 
     public void onTestStart(ITestResult iTestResult) {
-        log.info(("======================================== STARTING TEST " + iTestResult.getName() + " ========================================"));
 
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-       log.info(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
-                getExecutionTime(iTestResult)));
+
 
     }
 
     public void onTestFailure(ITestResult iTestResult) {
-       log.info(String.format("======================================= FAILED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
-                getExecutionTime(iTestResult)));
+
         takeScreenshot(iTestResult);
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
-        log.info(String.format("======================================== SKIPPING TEST %s ========================================%n", iTestResult.getName()));
         takeScreenshot(iTestResult);
     }
 
