9725bf7003d80e034b87174ea309e16dcc9588e0
Admin <maxich66@gmail.com>
2020-10-21 18:27:52 +0300

    fix(3.0.0-M5 not found)

--- src/test/java/tests/WorkoutTest.java
+++ src/test/java/tests/WorkoutTest.java
@@ -9,9 +9,9 @@ public class WorkoutTest extends BaseTest {
 
     @Test
     public void addWorkout() {
-        Bike bike = Bike.builder().build();
-
-
+        Bike bike = new Bike(driver, "10/23/2020", "05:45 AM", "bitka"
+                , "kachat' bitky", "01:00:22", "2:21", "min/mi",12,"km" ,"Poor",
+                "3 (Light)", 60, 70, 90, 2000);
         loginSteps
                 .login("masya@mail.ru", "1234321MAks__", false)
                 .toWorkoutPage();
