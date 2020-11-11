6197d285adc0c1dff4231639bb1844a5f0ed5b9f
Admin <maxich66@gmail.com>
2020-11-11 12:56:29 +0300

    test

--- src/test/java/pages/CalendarPage.java
+++ src/test/java/pages/CalendarPage.java
@@ -48,8 +48,8 @@ public class CalendarPage extends BasePage {
         Actions actions = new Actions(driver);
         WebElement calendarPlus = driver.findElement(By.xpath(format(CALENDAR_PLUS, day)));
         actions.moveToElement(calendarPlus).perform();
+        actions.moveToElement(calendarPlus).perform();
         calendarPlus.click();
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(format(SELECT_OPTION_INTO_DROPDOWN, day, option)))));
         driver.findElement(By.xpath(format(SELECT_OPTION_INTO_DROPDOWN, day, option))).click();
     }
 
